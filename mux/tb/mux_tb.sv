dad0679bbdb3e3491f08fc0d89dd08c516431333
Charlotte000 <ramil.mifteev@yandex.ru>
2025-10-29 16:57:18 +0300

    mux: add test_case function & random test values

--- mux/tb/mux_tb.sv
+++ mux/tb/mux_tb.sv
@@ -9,6 +9,28 @@ logic [1:0] direction_i;
 
 logic [1:0] data_o;
 
+task test_case(
+    input  logic [1:0] data,
+    input  logic [1:0] direction
+  );
+  case( direction )
+    2'b00:   data0_i = data;
+    2'b01:   data1_i = data;
+    2'b10:   data2_i = data;
+    2'b11:   data3_i = data;
+    default: data0_i = data;
+  endcase
+  direction_i = direction;
+
+  #10
+
+  if( data_o !== data )
+  begin
+    $display( "Test Failed: expected %b but got %b", data, data_o );
+    $stop;
+  end
+endtask
+
 mux DUT (
   .direction_0_i ( direction_i[0] ),
   .direction_1_i ( direction_i[1] ),
@@ -26,42 +48,8 @@ mux DUT (
 
 initial
   begin
-    data0_i <= 2'b11;
-    data1_i <= 2'b10;
-    data2_i <= 2'b01;
-    data3_i <= 2'b00;
-
-    direction_i <= 2'b00;
-    #10;
-    if( data_o != 2'b11 )
-      begin
-        $display( "Test Failed: expected %b but got %b", 2'b11, data_o );
-        $stop;
-      end
-
-    direction_i <= 2'b01;
-    #10;
-    if( data_o != 2'b10 )
-      begin
-        $display( "Test Failed: expected %b but got %b", 2'b10, data_o );
-        $stop;
-      end
-
-    direction_i <= 2'b10;
-    #10;
-    if( data_o != 2'b01 )
-      begin
-        $display( "Test Failed: expected %b but got %b", 2'b01, data_o );
-        $stop;
-      end
-
-    direction_i <= 2'b11;
-    #10;
-    if( data_o != 2'b00 )
-      begin
-        $display( "Test Failed: expected %b but got %b", 2'b00, data_o );
-        $stop;
-      end
+    for( int i = 0; i < 100; i++ )
+      test_case( $urandom_range(3, 0), $urandom_range(3, 0) );
 
     $display( "Tests Passed" );
   end
