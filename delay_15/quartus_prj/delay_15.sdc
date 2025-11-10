7144c057dbe55ff752eff84d96d440053161e1b9
Charlotte000 <ramil.mifteev@yandex.ru>
2025-11-10 14:39:29 +0300

    delay_15: fix clk pin assignment & fix sdc period & update tb

--- delay_15/quartus_prj/delay_15.sdc
+++ delay_15/quartus_prj/delay_15.sdc
@@ -1,5 +1,5 @@
 set_time_format -unit ns -decimal_places 3
 
-create_clock -name {clk_150} -period 150MHz [get_ports {clk_i}]
+create_clock -name {clk_150} -period 6.666 [get_ports {clk_i}]
 
 derive_clock_uncertainty
--- delay_15/tb/delay_15_tb.sv
+++ delay_15/tb/delay_15_tb.sv
@@ -23,21 +23,22 @@ task reset();
   ##0;
 endtask
 
-task test_case( input logic [3:0] data_delay );
-  reset();
-
+task test_case(
+  input logic       data,
+  input logic [3:0] data_delay
+);
   data_delay_i = data_delay;
 
   for( int i = 0; i < data_delay; i++ )
     begin
-      data_i = i == 0;
+      data_i = ( i == 0 ) ? ( data ) : ( !data );
       ##1;
     end
 
   ##1;
-  if( data_o !== 1 )
+  if( data_o !== data )
     begin
-      $display( "Test Failed: ( data_delay_i = %b ) expected %b but got %b", data_delay_i, 1'b1, data_o );
+      $display( "Test Failed: ( data_delay_i = %b ) expected %b but got %b", data_delay_i, data, data_o );
       $stop;
     end
 endtask
@@ -55,7 +56,7 @@ initial
     data_delay_i = 4'b0001;
 
     for( int i = 0; i < 100; i++ )
-      test_case( $urandom_range(15, 1) );
+      test_case( $urandom_range(1, 0), $urandom_range(15, 1) );
 
     $display( "Tests Passed" );
     $stop;
