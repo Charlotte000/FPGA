4feac61debe635af157ca77c504fe52b9537c733
Charlotte000 <ramil.mifteev@yandex.ru>
2025-11-15 16:09:28 +0300

    crc_16_ansi: remove automatic & use nba & converge algo to CRC-16/BUYPASS

--- crc_16_ansi/tb/crc_16_ansi_tb.sv
+++ crc_16_ansi/tb/crc_16_ansi_tb.sv
@@ -1,10 +1,19 @@
+/*
+CRC-16/BUYPASS
+- Width:  16
+- Poly:   16'h8005
+- Init:   16'h0000
+- RefIn:  false
+- RefOut: false
+- XorOut: 16'h0000
+*/
 `timescale 1ns/1ps
 
-typedef logic [15:0] crc_16_hash;
+typedef logic [15:0] crc_16_checksum;
 
 module crc_16_ansi_tb #(
   parameter PERIOD   = 6.666,
-  parameter POLY     = 16'h8005,
+  parameter CRC_POLY     = 16'h8005,
   parameter CRC_INIT = 16'h0000,
   parameter DATA_W   = 32
 );
@@ -13,7 +22,7 @@ bit                clk;
 bit                rst;
 
 logic              data_i;
-crc_16_hash        data_o;
+crc_16_checksum    data_o;
 
 logic [DATA_W-1:0] data;
 
@@ -24,29 +33,29 @@ crc_16_ansi DUT (
   .data_o ( data_o )
 );
 
-function void crc_16_step( input logic data, inout crc_16_hash hash );
-  hash = ( data ^ ( ( hash >> 15 ) & 1 ) ) ? ( ( hash << 1 ) ^ POLY ) : ( hash << 1 );
+function void crc_16_step( input logic data, inout crc_16_checksum checksum );
+  checksum = ( data ^ ( ( checksum >> 15 ) & 1 ) ) ? ( ( checksum << 1 ) ^ CRC_POLY ) : ( checksum << 1 );
 endfunction
 
-function automatic crc_16_hash crc_16( input logic [DATA_W-1:0] data );
-  crc_16_hash result;
+function crc_16_checksum crc_16( input logic [DATA_W-1:0] data );
+  crc_16_checksum result;
   result = CRC_INIT;
 
-  for( int i = 0; i < DATA_W; i++ )
+  for( int i = DATA_W - 1; i >= 0; i-- )
     crc_16_step( data[i], result );
 
   return result;
 endfunction
 
-task automatic test_case();
-  crc_16_hash result_true;
+task test_case();
+  crc_16_checksum result_true;
 
   reset();
 
   // Feed data
-  for( int i = 0; i < DATA_W; i++ )
+  for( int i = DATA_W - 1; i >= 0; i-- )
     begin
-      data_i = data[i];
+      data_i <= data[i];
       @( posedge clk );
     end
 
@@ -61,9 +70,9 @@ endtask
 
 task reset();
   @( posedge clk );
-  rst = 1;
+  rst <= 1;
   @( posedge clk );
-  rst = 0;
+  rst <= 0;
 endtask
 
 initial
